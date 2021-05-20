1b1bb58f56e816976add8af99767a9b7c9fa2660
faozziyyah <65113702+faozziyyah@users.noreply.github.com>
2021-05-20 04:35:13 +0100

    uploaded all files

---
+++ index.js
@@ -0,0 +1,182 @@
+
+// to display the first details
+let name0 = document.getElementById("name0");
+let about0 = document.getElementById("about0");
+let teacher0 = document.getElementById("teacher0");
+
+about0.style.display = "none";
+name0.addEventListener("click", showAbout0);
+//teacher0.addEventListener("click", hideAbout0);
+
+function showAbout0 (){
+   // about0.style.display = "block";
+    //name0.style.display = "none";
+    if (about0.style.display === "none") {
+        about0.style.display = "block";
+        name0.style.display = "none";
+    } else {
+        about0.style.display = "none";
+    }
+}
+
+// to display the second details
+let name1 = document.getElementById("name1");
+let about1 = document.getElementById("about1");
+
+about1.style.display = "none";
+name1.addEventListener("click", showAbout1);
+
+function showAbout1 (){
+   // about0.style.display = "block";
+    //name0.style.display = "none";
+    if (about1.style.display === "none") {
+        about1.style.display = "block";
+        name1.style.display = "none";
+    } else {
+        about1.style.display = "none";
+    }
+}
+
+// to display the third details
+let name2 = document.getElementById("name2");
+let about2 = document.getElementById("about2");
+
+about2.style.display = "none";
+name2.addEventListener("click", showAbout2);
+
+function showAbout2 (){
+   // about0.style.display = "block";
+    //name0.style.display = "none";
+    if (about2.style.display === "none") {
+        about2.style.display = "block";
+        name2.style.display = "none";
+    } else {
+        about2.style.display = "none";
+    }
+}
+
+// to display the fourth details
+let name3 = document.getElementById("name3");
+let about3 = document.getElementById("about3");
+
+about3.style.display = "none";
+name3.addEventListener("click", showAbout3);
+
+function showAbout3 (){
+   // about0.style.display = "block";
+    //name0.style.display = "none";
+    if (about3.style.display === "none") {
+        about3.style.display = "block";
+        name3.style.display = "none";
+    } else {
+        about3.style.display = "none";
+    }
+}
+
+// to display the fifth details
+let name4 = document.getElementById("name4");
+let about4 = document.getElementById("about4");
+
+about4.style.display = "none";
+name4.addEventListener("click", showAbout4);
+
+function showAbout4 (){
+   // about0.style.display = "block";
+    //name0.style.display = "none";
+    if (about4.style.display === "none") {
+        about4.style.display = "block";
+        name4.style.display = "none";
+    } else {
+        about4.style.display = "none";
+    }
+}
+
+// to display the sixth details
+let name5 = document.getElementById("name5");
+let about5 = document.getElementById("about5");
+
+about5.style.display = "none";
+name5.addEventListener("click", showAbout5);
+
+function showAbout5 (){
+   // about0.style.display = "block";
+    //name0.style.display = "none";
+    if (about5.style.display === "none") {
+        about5.style.display = "block";
+        name5.style.display = "none";
+    } else {
+        about5.style.display = "none";
+    }
+}
+
+// to display the seventh details
+let name6 = document.getElementById("name0");
+let about6 = document.getElementById("about6");
+
+about6.style.display = "none";
+name6.addEventListener("click", showAbout6);
+
+function showAbout6 (){
+   // about0.style.display = "block";
+    //name0.style.display = "none";
+    if (about6.style.display === "none") {
+        about6.style.display = "block";
+        name6.style.display = "none";
+    } else {
+        about6.style.display = "none";
+    }
+}
+
+// to display the eight details
+let name7 = document.getElementById("name7");
+let about7 = document.getElementById("about7");
+
+about7.style.display = "none";
+name7.addEventListener("click", showAbout7);
+
+function showAbout7 (){
+   // about0.style.display = "block";
+    //name0.style.display = "none";
+    if (about7.style.display === "none") {
+        about7.style.display = "block";
+        name7.style.display = "none";
+    } else {
+        about7.style.display = "none";
+    }
+}
+
+// to display the ninth details
+let name8 = document.getElementById("name8");
+let about8 = document.getElementById("about8");
+
+about8.style.display = "none";
+name8.addEventListener("click", showAbout8);
+
+function showAbout8 (){
+   // about0.style.display = "block";
+    //name0.style.display = "none";
+    if (about8.style.display === "none") {
+        about8.style.display = "block";
+        name8.style.display = "none";
+    } else {
+        about8.style.display = "none";
+    }
+}
+
+// to display the tenth details
+let name9 = document.getElementById("name9");
+let about9 = document.getElementById("about9");
+
+about9.style.display = "none";
+name9.addEventListener("click", showAbout9);
+
+function showAbout9 (){
+   // about0.style.display = "block";
+    //name0.style.display = "none";
+    if (about9.style.display === "none") {
+        about9.style.display = "block";
+        name9.style.display = "none";
+    } else {
+        about9.style.display = "none";
+    }
+}
